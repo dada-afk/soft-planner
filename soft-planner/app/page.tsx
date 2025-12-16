b66681e958ee240eac723097afff7886ba857aa6
dada-afk <daniellarus99@gmail.com>
2025-12-16 21:46:11 +0100

    UI component refactor and cleanup
    refactor: Button component,
    add: Typography, FormField and Text component

--- soft-planner/app/page.tsx
+++ soft-planner/app/page.tsx
@@ -1,17 +1,21 @@
 'use client';
 import { Button } from "@/ui/Button/Button";
 import { UIIcons } from "@/ui/Icons/Icons";
-import { UIIconPositions } from "@/ui/UIEnums/UIEnums";
+import { Text } from "@/ui/Inputs/Text";
+import { Typography } from "@/ui/Typography/Typography";
+import { FontSize, FontWeight, UIButtonVariant, UIIconPositions } from "@/ui/ui-system/ui-system";
+import { Rating } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
-  const test = () => {
-    console.log('test');
-  }
+  const [inputValue, setInputValue] = useState('');
 
   return (
     <>
-      <h1>This is the main page</h1>
-      <Button onClick={test} text={'Add'} icon={UIIcons.Add} iconPosition={UIIconPositions.End} />
+      <Typography text={'This is the main page'} fontSize={FontSize.md} fontWeight={FontWeight[500]} />
+      <Text id={'1'} value={inputValue} onChange={setInputValue} label={'Label'} error={'Something went wrong.'} placeholder={'Placeholder text'}>
+        <Button onClick={() => console.log('hi')} icon={UIIcons.add} iconPosition={UIIconPositions.End} variant={UIButtonVariant.Secondary} />
+      </Text>
     </>
   );
 }
--- soft-planner/ui/Icons/Icons.tsx
+++
@@ -1,7 +0,0 @@
-import AddIcon from '@mui/icons-material/Add';
-
-export const UIIcons = {
-    Add: AddIcon,
-};
-
-export type UIIconsType = typeof UIIcons[keyof typeof UIIcons];
